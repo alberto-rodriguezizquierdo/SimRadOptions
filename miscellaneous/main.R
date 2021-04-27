ea2a6e945d02f43f0b14af88828066c3dc10d835
Alberto_win <alberto.rodriguez@inia.es>
2021-04-27 10:29:07 +0200

    Main of SimRADoptionsApp

---
+++ miscellaneous/main.R
@@ -0,0 +1,33 @@
+#test libraries
+
+rm(list=ls())
+
+libInstalling <- c('dplyr','XML','tidyverse', 'SimRAD')
+
+is.installed <- function(paquete) is.element(paquete, installed.packages())
+
+for (packages in libInstalling){
+  if (!is.installed(packages)){
+
+    install.packages(packages)
+  }
+  print (packages)
+  library(packages,character.only=TRUE)
+}
+
+
+#Setting Working directories
+root                  <- "C:/Users/David/Desktop/SimRadOptions/"
+setwd(root)
+rDirectory            <- 'R/'
+myDirectoryR          <- paste0(root,rDirectory)
+
+
+listFiles <- list.files(myDirectoryR, pattern = ".R")
+for (rscripts in listFiles){
+  source(paste0(myDirectoryR,rscripts))
+}
+
+#noiseqApp(root)
+debug(SimRADoptionsApp(root))
+#loadingScripts   <- source(myDirectoryR,)
